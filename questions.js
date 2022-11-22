d2f414b9e065835296817f08866a4255f4a71ff7
April Larsen <hunterapril94@gmail.com>
2022-11-22 11:36:54 -0800

    write questions

---
+++ questions.js
@@ -0,0 +1,221 @@
+export const questions = {
+  // GBBO A = pastry, B = Cake, C= Bread, D = Biscuit
+  1: {
+    1: {
+      question: 'Where would you rather be on a Friday night?',
+      A: 'Eating at that new expensive restaurant that opened up',
+      B: 'Partying!',
+      C: 'Home watching Netflix',
+      D: 'Wherever my people want to be'
+    },
+    2: {
+      question: 'What outfit do you choose to go out?',
+      A: 'Little Black Dress and pumps',
+      B: 'Something with lots of color and personality',
+      C: 'Whatever is comfy',
+      D: 'It really depends on where I am going'
+    },
+    3: {
+      question: 'What is your favorite color?',
+      A: 'Black',
+      B: 'Rainbow',
+      C: 'Brown',
+      D: 'Green'
+    },
+    4: {
+      question: 'What word would people use to describe you?',
+      A: 'Classic',
+      B: 'Fun',
+      C: 'Cozy',
+      D: 'Relatable'
+    },
+    5: {
+      question: 'Where is your dream vacation spot?',
+      A: 'Big city',
+      B: 'Amusement Park',
+      C: 'Log Cabin',
+      D: 'Beach'
+    },
+    6: {
+      question: 'What is your go to show on Netflix?',
+      A: 'The Crown',
+      B: 'Glow Up',
+      C: 'Parks and Rec',
+      D: 'The Queens Gambit'
+    },
+    // 7: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // },
+    // 8: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // },
+    // 9: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // },
+    // 10: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // }
+  },
+  // Colors A = pink, B = Yellow, C = Green, D = Blue
+  2: {
+    1: {
+      question: 'What would you do in a natural disaster?',
+      A: 'Go around and ask if anyone needs anything',
+      B: 'Start brainstorming ideas for how to take care of things',
+      C: 'Take charge and give out assignments',
+      D: 'Hand out blankets and bottles of water'
+    },
+    2: {
+      question: 'What is your favorite type of cheese?',
+      A: 'Cheddar',
+      B: 'Gouda',
+      C: 'Mozarella',
+      D: 'Parmesan'
+    },
+    3: {
+      question: 'What Christmas movie will you watch?',
+      A: 'The latest Hallmark movie',
+      B: 'A Christmas story',
+      C: 'Scrooge',
+      D: 'Its a Wonderful Life'
+    },
+    4: {
+      question: 'Choose an interior design trend',
+      A: 'Family picture gallery wall',
+      B: 'Bright Colored cabinets',
+      C: 'Greenhouse room',
+      D: 'Labeled jars in the pantry'
+    },
+    5: {
+      question: 'Choose a dog breed',
+      A: 'Labrador',
+      B: 'Golden Retriever',
+      C: 'German Shepherd',
+      D: 'St Bernhard'
+    },
+    6: {
+      question: 'Choose a Star Wars Character',
+      A: 'Luke',
+      B: 'Han Solo',
+      C: 'Princess Leia',
+      D: 'C-3P0'
+    },
+    // 7: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // },
+    // 8: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // },
+    // 9: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // },
+    // 10: {
+    //   question: '',
+    //   A: '',
+    //   B: '',
+    //   C: '',
+    //   D: ''
+    // }
+  },
+  // Dog Breed A = Husky, B = Daschund, C = Boston Terrier, D = Yorkshire Terrier
+  // 3: {
+  //   1: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   2: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   3: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   4: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   5: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   6: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   7: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   8: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   9: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   },
+  //   10: {
+  //     question: '',
+  //     A: '',
+  //     B: '',
+  //     C: '',
+  //     D: ''
+  //   }
+  // }
+}
