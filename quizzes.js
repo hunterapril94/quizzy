63f39caf9b3ffe63ad046efcc0bee16fec2b7989
April Larsen <hunterapril94@gmail.com>
2022-11-22 10:17:10 -0800

    create react app and quiz descriptions

---
+++ quizzes.js
@@ -0,0 +1,87 @@
+export const quizzes = {
+  1: {
+    title: 'What Great British Bake Off week are you?',
+    answers: {
+      A:{
+        title: 'Pastry Week',
+        description: "You're classic and a little high maintenance. People who don't know you well call you difficult, but those that take the time to know you know you're the most loyal friend possible. Your favorite activity is brunch at the fanciest restaurant available."
+      } ,
+      B: {
+        title: 'Cake Week',
+        description: "You're easy going and outgoing. You often dress pretty flamboyantly and have a big personality and are always a favorite at parties."
+      },
+      C: {
+        title: 'Bread Week',
+        description: "You give the best hugs. You're a natural at anything. You like spending a night at home snuggled up on the couch"
+      },
+      D: {
+        title: 'Biscuit Week',
+        description: "On the surface you seem like the person everyone can relate to. Once people get to know you they realize you have a complex inner world. You're an ambivert and are equally likely to be found partying or at home on a Friday night."
+      }
+    }    
+  },
+  2: {
+    title: "What color should you wear today?",
+    answers: {
+      A: {
+        title: 'Pink',
+        description: 'You are loving, kind, generous and sensitive to the needs of others. And your clothes should reflect that.'
+      },
+      B: {
+        title: 'Yellow',
+        description: 'You are creative, often being the one who comes up with new ideas - an ideas person who needs others to bring the ideas into reality - you tend to have your head in the clouds much of the time. And your outfit should show that off.'
+      },
+      C: {
+        title: 'Green',
+        description: 'you are kind, generous and compassionate - good to have around during a crisis as you remain calm and take control of the situation until it is resolved. And your outfit should reflect that.'
+      },
+      D: {
+        title: 'Blue',
+        description: 'You are genuine and sincere, and you take your responsibilities seriously. And your clothes should reflect that.'
+      }
+    }
+
+  },
+  3: {
+    title: 'What Dog Breed Are You?',
+    answers: {
+      A: {
+        title: 'Husky',
+        description: "not afraid to voice your feelings. When there's an injustice you're the first person to point it out."
+      },
+      B: {
+        title: 'Daschund',
+        description: 'brave, loyal, and a bit stubborn while being friendly enough to win over just about anyone.'
+      },
+      C: {
+        title: 'Boston Terrier',
+        description: 'silly and upbeat. Adaptable and happy in lots of circumstances.'
+      },
+      D: {
+        title: '',
+        description: 'feisty, take-charge personality. Big personality in small, cuddly package.'
+      }
+    }
+  },
+  // 4: {
+  //   title: '',
+  //   answers: {
+  //     A: {
+  //       title: '',
+  //       description: ''
+  //     },
+  //     B: {
+  //       title: '',
+  //       description: ''
+  //     },
+  //     C: {
+  //       title: '',
+  //       description: ''
+  //     },
+  //     D: {
+  //       title: '',
+  //       description: ''
+  //     }
+  //   }
+  // }
+}
